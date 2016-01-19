3f6014589d7d519dcd6385f8182efe49e4ec2f6f
Johanna Delanoy <johanna.delanoy@inria.fr>
2016-01-19 22:01:06 +0100

    script for plotting loss

---
+++ plot_loss.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+
+train = np.load('/home/caffe_snapshot/train_loss_data_sketch.npy')
+test = np.load('/home/caffe_snapshot/test_loss_data_sketch.npy')
+accu = np.load('/home/caffe_snapshot/accuracy_data_sketch.npy')
+
+niter=train.shape[0]
+#test = test[:4500/50]
+test_iter=niter/test.shape[0]
+train_iter=10
+
+plt.plot(range(0,niter,train_iter),train[0:niter:train_iter])
+plt.plot(range(0,niter,test_iter),test)
+#plt.plot(range(0,niter,test_iter),accu)
+plt.show()
+
+
+
